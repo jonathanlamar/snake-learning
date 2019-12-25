Fixing variance test, writing RNG test, debugging seeding of RNG.

game/game_state.py
@@ -79,7 +79,7 @@ class GameState(InitConfig):
         # Add prize cell
         self.board[self.prize_loc[0], self.prize_loc[1]] = -1
 
-    def _get_new_prize_loc(self)
+    def _get_new_prize_loc(self):
         X, Y = np.where(self.board == 0)
         i = np.random.choice(range(len(X)))
         return np.array([X[i], Y[i]])

tests/test_generation.py
@@ -9,19 +9,19 @@ def test_load_generation():
         return all([np.all(x == y) for x, y in zip(P.model.get_weights(),
                                                    Q.model.get_weights())])
 
-    G = Generation()
+    G = Generation(generation_size=10)
     G.spawn_random()
     G.eval_players()
     G.save_latest_gen(test=True)
 
-    H = Generation()
+    H = Generation(generation_size=10)
     H.load_latest_gen(test=True)
 
     assert all([player_equality(P, Q)
                 for P, Q in zip(G.players, H.players)])
 
 def test_repeat_performance():
-    G = Generation()
+    G = Generation(generation_size=10)
     G.spawn_random()
     G.eval_players()
 

tests/test_player.py
@@ -71,7 +71,7 @@ def test_fuzz_var():
 
     # This will fail with nonzero probability
     config = InitConfig()
-    assert np.abs(arr_fuzz.var() - config.mutation_rate) < 0.01
+    assert np.abs(arr_fuzz.std() - config.mutation_rate) < 0.01
 
 def test_save_and_load():
     P = Player()