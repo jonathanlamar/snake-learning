56e120d31c6d0f575b092b788de99c920c4d4518
Jonathan Lamar <jonathan.lamar@charter.com>
2019-12-26 11:51:13 -0600

    Added shell script for training one iter.

---
+++ train_one.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# NOTE: You must spawn one generation, eval, and save before this script can be
+# run.  This will breed the next generation, eval, and save.
+
+from ai.generation import Generation
+
+if __name__ == '__main__':
+    gen = Generation()
+    gen.load_latest_gen()
+    gen.train_iter(1)
