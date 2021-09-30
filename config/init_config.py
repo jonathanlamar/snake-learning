c9172f40628abd278fce5cf92e9923e648fcaa40
Jon <jonathan.p.lamar@gmail.com>
2021-09-30 12:36:20 -0600

    Cant recreate previous performance.  What gives...

--- config/init_config.py
+++ config/init_config.py
@@ -15,7 +15,7 @@ class InitConfig:
         # Number of top players to breed next generation
         self.number_to_breed = 10
         # Standard deviation of Gaussian noise added during breeding algorithm
-        self.mutation_rate = 0.1
+        self.mutation_rate = 0.3
         # Take average of this many games to select best players
         self.num_games_to_play = 1
 
@@ -25,14 +25,14 @@ class InitConfig:
         # Each new score allows this many more frames
         self.extra_time_per_score = 100
         # Overall max frames
-        self.max_time_allowed = 2000
+        self.max_time_allowed = 1000
 
         # How big should the game be?
         self.board_size = 30
 
         # The neural network will have an input layer of size 24, output layer
         # of size 4, and num_hidden_layers input layers of size hidden_layer_size
-        self.num_hidden_layers = 3
+        self.num_hidden_layers = 1
         self.hidden_layer_size = 18
 
     def fitness_function(self, score: int, duration: int) -> float:
