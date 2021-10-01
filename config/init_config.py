814896f3eb9387d0b373e0d8d1458fd6a0623c4b
Jon <jonathan.p.lamar@gmail.com>
2021-10-01 07:10:46 -0600

    Okay, done messing around with genetic algorithm.

--- config/init_config.py
+++ config/init_config.py
@@ -15,7 +15,7 @@ class InitConfig:
         # Number of top players to breed next generation
         self.number_to_breed = 10
         # Standard deviation of Gaussian noise added during breeding algorithm
-        self.mutation_rate = 0.3
+        self.mutation_rate = 0.2
         # Take average of this many games to select best players
         self.num_games_to_play = 1
 
@@ -25,18 +25,18 @@ class InitConfig:
         # Each new score allows this many more frames
         self.extra_time_per_score = 100
         # Overall max frames
-        self.max_time_allowed = 1000
+        self.max_time_allowed = 3000
 
         # How big should the game be?
         self.board_size = 30
 
         # The neural network will have an input layer of size 24, output layer
         # of size 4, and num_hidden_layers input layers of size hidden_layer_size
-        self.num_hidden_layers = 1
+        self.num_hidden_layers = 2
         self.hidden_layer_size = 18
 
     def fitness_function(self, score: int, duration: int) -> float:
         # This is the fitness function for the selection algorithm.
         # Generation instances will use this function to decide fitness.
 
-        return np.log(1 + duration) + 2 * score
+        return 2 * np.log(1 + duration) + score
